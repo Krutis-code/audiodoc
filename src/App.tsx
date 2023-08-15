427e11a33cd501cc7cc1b0acf1d719b32663bc21
kruti patel <krutipatel1262@gmail.com>
2023-08-15 22:24:25 +0530

    click handler for paragraph

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import * as React from "react";
+
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import transcript from "./dummyData";
 
 function App() {
-  const playerRef:any = useRef<HTMLAudioElement>(null);
-  const wordsRef:any = useRef<HTMLSpanElement>(null);
+  const [current, setCurrent] = useState<any>(null);
+  const [prevData, setPrevData] = useState<any>(null);
+  const playerRef: any = useRef<HTMLAudioElement>(null);
+  const wordsRef: any = useRef<HTMLSpanElement>(null);
+
   // const [prev, setPrev] = useState<any>(null);
   useEffect(() => {
     const onTimeUpdate = () => {
@@ -13,52 +18,90 @@ function App() {
       });
       console.log(activeWordIndex);
       // let i = 0;
-      // while(i<=activeWordIndex-2){
+      // while (i <= activeWordIndex - 2) {
       //   const prevNodes = wordsRef.current.childNodes[i];
-      //   prevNodes.classList.remove('active-word');
+      //   prevNodes.classList.remove("active-word");
       //   i++;
       // }
-      if(activeWordIndex >= 2){
-        let prev = wordsRef.current.childNodes[activeWordIndex-2];
-        prev.classList.remove('active-word');
+      if (activeWordIndex >= 2) {
+        let prev = wordsRef.current.childNodes[activeWordIndex - 2];
+        prev.classList.remove("active-word");
       }
-      const wordElement:any = wordsRef && wordsRef.current && wordsRef.current.childNodes[activeWordIndex-1];
-      if(wordElement){
-        wordElement.classList.add('active-word');
+      const wordElement: any =
+        wordsRef &&
+        wordsRef.current &&
+        wordsRef.current.childNodes[activeWordIndex - 1];
+      if (wordElement) {
+        wordElement.classList.add("active-word");
+        console.log(wordElement);
       }
     };
     playerRef.current.addEventListener("timeupdate", onTimeUpdate);
-    return () => playerRef.current.removeEventListener(
-      "timeupdate",
-        onTimeUpdate
-    );
+    return () =>
+      playerRef.current.removeEventListener("timeupdate", onTimeUpdate);
   }, []);
-  
-  const handleSelected = (param: number) => {
-    console.log(window.getSelection()?.toString());
-  }
+
+  const handleSelected = (param: number, data: any) => {
+    console.log("data", param, data);
+    if (current !== null) {
+      const activeWordIndex = transcript.blocks.findIndex((value) => {
+        return value == current;
+      });
+      const prevNodes = wordsRef.current.childNodes[activeWordIndex];
+      console.log("prevNode", prevNodes);
+      prevNodes.classList.remove("active-word");
+    }
+
+    if (data != current || current === null) {
+      const activeWordIndex = transcript.blocks.findIndex((value) => {
+        return value == data;
+      });
+      console.log("value", activeWordIndex);
+      if (activeWordIndex > -1) setCurrent(data);
+      console.log(
+        "playerRef.current.currentTime",
+        playerRef.current.currentTime
+      );
+      console.log("playerRef.current", playerRef.current);
+      playerRef.current.currentTime = data.start;
+      playerRef.current.play();
+    }
+  };
 
   return (
     <>
       <h1>{transcript.title}</h1>
-      {/* <div onDoubleClick={() => handleSelected(1)} onMouseUp={() => handleSelected(2)} onClick={() => handleSelected(4)}>
-        {transcript.blocks.map((item: any, index: number) => { 
-          return(
-          <div>
-            <p>block- {`${index}`}</p>
-            <p 
-            key={index}>{item.text}
+      <div
+        ref={wordsRef}
+        // onDoubleClick={() => handleSelected(1)}
+        // onMouseUp={() => handleSelected(2)}
+        // onClick={() => handleSelected(4)}
+      >
+        {transcript.blocks.map((item: any, index: number) => {
+          return (
+            <p
+              key={index}
+              // onDoubleClick={() => handleSelected(1, item)}
+              // onMouseUp={() => handleSelected(2, item)}
+              onClick={() => handleSelected(4, item)}
+            >
+              {item.text}
             </p>
-          </div>)
+          );
         })}
-      </div> */}
+      </div>
 
-      <ol ref={wordsRef}>
+      {/* <ol ref={wordsRef}>
         {transcript.blocks.map((word, i) => <li key={i}>{word.text}</li>)}
       </ol>
-      
-      <audio controls controlsList="nodownload" style={{ width: '100vw'}} ref={playerRef}>
-        <source src={transcript.audioUrl} type="audio/ogg"  />
+       */}
+      <audio
+        controls
+        controlsList="nodownload"
+        style={{ width: "100vw", position: "sticky", bottom: 0 }}
+        ref={playerRef}
+      >
+        <source src={transcript.audioUrl} type="audio/ogg" />
         {/* <source src={} type="audio/mpeg" /> */}
       </audio>
     </>
